e932b6677023ef7beb3e57391ba6dec3b87a17fc
Alexander Frick <Alex313031@gmail.com>
2024-04-15 05:21:00 -0500

    fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ["./node_modules/@balena/lint/config/.eslintrc.js"],
   root: true,
   ignorePatterns: ["node_modules/"],
+  ignorePatterns: ["lib/shared/typings/*"],
   rules: {
     "@typescript-eslint/no-floating-promises": "off",
     "@typescript-eslint/no-var-requires": "off",
